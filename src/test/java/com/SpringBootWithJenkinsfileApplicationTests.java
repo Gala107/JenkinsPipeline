b5ecc3a0afeafd033cccacdbb450d9893681f8e4
Gala107 <wgalina@hotmail.com>
2024-02-29 20:47:10 -0600

    fixed errors

--- src/test/java/com/SpringBootWithJenkinsfileApplicationTests.java
+++ src/test/java/com/SpringBootWithJenkinsfileApplicationTests.java
@@ -1,7 +1,5 @@
 package com;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,7 +8,7 @@ class SpringBootWithJenkinsfileApplicationTests {
 
 	@Test
 	void contextLoads() {
-		assertEquals("mymy", "hello");
+		
 	}
 
 }
